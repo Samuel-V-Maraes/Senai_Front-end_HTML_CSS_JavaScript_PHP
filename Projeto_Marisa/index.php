0c0531b45683028a378555fdc539f196038e3165
Samuel-V-Maraes <samuelmaraes9@gmail.com>
2026-04-10 18:59:30 -0300

    Add initial index.php for Marisa website
    This is the initial commit of the index.php file containing the HTML structure for the Marisa website, including sections for categories, images, and promotional content.

---
+++ Projeto_Marisa/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--conexeção-->
+    <style type="text/css">@import url("css/config_ini.css");</style>
+
+    <title>Marisa - seja bem vindo ao nosso site!</title>
+</head>
+<body>
+    <?php include("includes/topo.php");?>
+    <!--Fim - menu principal-->
+
+    <!--inicio - publicidade de frete gratís no Brasil -->
+    <div class="frete">
+        <img src="img/frete.gif" alt="" />
+    </div>
+    <!--Fim - publicidade de frete gratís no Brasil -->
+
+    <!--inicio - imagens dentro do carrosel-->
+    <div class="banner"></div>
+    <!--Fim - imagens dentro do carrosel-->
+
+    <div class="topbar">
+        <div class="top-item">
+            <img src="img/GRÁTIS.gif" alt="" class="icon"/>
+            <a href="" class="infor_link">FRETE GRÁTIS BRASIL</a>
+        </div>
+
+        <div class="top-item">
+            <img src="img/FATURA.gif" alt="" class="icon"/>
+            <a href="" class="infor_link">PAGAMENTO DE FATURA</a>
+        </div>
+
+        <div class="top-item">
+            <img src="img/COMPRE.gif" alt="" class="icon"/>
+            <a href="" class="infor_link">COMPRE NO SITE TROQUE NA LOJA</a>
+        </div>
+
+        <div class="top-item">
+            <img src="iMG/JUROS.gif" alt="" class="icon" />
+            <a href="" class="infor_link">ATÉ 5X SEM JUROS</a>
+        </div>
+    </div>
+    
+    <section class="categorias">
+
+  <p class="titulo">NAVEGUE POR CATEGORIA</p>
+
+  <div class="lista-categorias">
+
+    <div class="item">
+      <div class="circulo destaque">%</div>
+      <p>Ofertas</p>
+    </div>
+
+    <div class="item">
+      <img src="img/FEMININOgif.gif">
+      <p>Feminino</p>
+    </div>
+
+    <div class="item">
+      <img src="img/CALÇADOS.gif">
+      <p>Calçados</p>
+    </div>
+
+    <div class="item">
+      <img src="img/INFANTIL.gif">
+      <p>Infantil</p>
+    </div>
+
+    <div class="item">
+      <img src="img/LINGERIE.gif">
+      <p>Lingerie</p>
+    </div>
+
+    <div class="item">
+      <img src="img/JEANS.gif">
+      <p>Jeans</p>
+    </div>
+
+    <div class="item">
+      <img src="img/PLUZ SIZE.gif">
+      <p>Plus Size</p>
+    </div>
+
+    <div class="item">
+      <img src="img/MASCULINO.gif">
+      <p>Masculino</p>
+    </div>
+
+    <div class="item">
+      <img src="img/ACESSORIOS.gif">
+      <p>Acessórios</p>
+    </div>
+
+  </div>
+
+</section>
+
+    <div>
+        <img src="img/imagem1.gif" alt="">
+    </div>
+ 
+    <div>
+        <img src="img/imagem2-1.gif" alt="">
+
+        <img src="img/imagem2-2.gif" alt="">
+
+    </div>
+
+    <div>
+        <img src="img/imagem3.gif" alt="">
+    </div>
+
+    <div>
+        <img src="img/imagem4-1.gif" alt="">
+
+        <img src="img/imagem4-2.gif" alt="">
+
+    </div>
+
+    <div>
+        <img src="img/imagem5.gif" alt="">
+    </div>
+
+    <div>
+        <img src="img/imagem6-1.gif" alt="">
+        <img src="img/imagem6-2.gif" alt="">
+    </div>
+
+    <div>
+        <img src="img/imagem7.gif" alt="">
+    </div>
+
+    <div>
+        <img src="img/imagem8-1.gif" alt="">
+        <img src="img/imagem8-2.gif" alt="">
+    </div>
+
+    <div>
+        <img src="img/imagem9.gif" alt="">
+    </div>
+
+    <div>
+        <img src="img/imagem10-1.gif" alt="">
+        <img src="img/imagem10-2.gif" alt="">
+    </div>
+
+    <div class="marcas">
+
+        <div class="marca_item">
+            <a href="#" class="marca_nome">ROVITEX</a>
+            <img src="img/rovitex.gif" alt="">
+        </div>
+            
+        <div class="marca_item">
+            <a href="#" class="marca_nome">SALVATORE</a>
+            <img src="img/salvatore.gif" alt="">
+        </div>
+        
+        <div class="marca_item">
+            <a href="#" class="marca_nome">CONCEPT LINGERIE</a>
+            <img src="img/concept lingerie.gif" alt="">
+        </div>
+
+        <div class="marca_item">
+            <a href="#" class="marca_nome">DUDA MAKEUP</a>
+            <img src="img/duda_makeup.gif" alt="">
+        </div>
+
+    </div>
+     
+    <?php include("includes/rodape.php"); ?>
+ 
+
+</body>
+</html>
